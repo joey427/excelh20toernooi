3ce7c55b8566331697e551e0004249382b815c6f
joey427 <joey@h20.gg>
2026-05-22 19:27:32 +0200

    Excel factory reset uitgebreid - toolbar, XLSTART, registry, account blijft bewaard

--- wis_appdata.bat
+++ wis_appdata.bat
@@ -32,7 +32,7 @@ echo [*] Bezig met wissen...
 echo.
 
 :: --- Tijdelijke bestanden gebruiker ---
-echo [1/14] Tijdelijke bestanden gebruiker (%TEMP%)...
+echo [1/13] Tijdelijke bestanden gebruiker (%TEMP%)...
 if exist "%TEMP%" (
     rd /s /q "%TEMP%" 2>nul
     md "%TEMP%" 2>nul
@@ -43,20 +43,20 @@ if exist "%TMP%" (
 )
 
 :: --- Windows Temp map ---
-echo [2/14] Windows Temp map (C:\Windows\Temp)...
+echo [2/13] Windows Temp map (C:\Windows\Temp)...
 if exist "C:\Windows\Temp" (
     rd /s /q "C:\Windows\Temp" 2>nul
     md "C:\Windows\Temp" 2>nul
 )
 
 :: --- Prefetch bestanden (vereist admin) ---
-echo [3/14] Prefetch bestanden...
+echo [3/13] Prefetch bestanden...
 if exist "C:\Windows\Prefetch" (
     del /f /q "C:\Windows\Prefetch\*.*" 2>nul
 )
 
 :: --- Recente bestanden ---
-echo [4/14] Recente bestanden...
+echo [4/13] Recente bestanden...
 if exist "%APPDATA%\Microsoft\Windows\Recent" (
     del /f /q "%APPDATA%\Microsoft\Windows\Recent\*.*" 2>nul
     del /f /q "%APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*.*" 2>nul
@@ -64,13 +64,13 @@ if exist "%APPDATA%\Microsoft\Windows\Recent" (
 )
 
 :: --- Thumbnail cache ---
-echo [5/14] Thumbnail cache...
+echo [5/13] Thumbnail cache...
 if exist "%LOCALAPPDATA%\Microsoft\Windows\Explorer" (
     del /f /q "%LOCALAPPDATA%\Microsoft\Windows\Explorer\thumbcache_*.db" 2>nul
 )
 
 :: --- Google Chrome cache ---
-echo [6/14] Google Chrome cache...
+echo [6/13] Google Chrome cache...
 if exist "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Cache" (
     rd /s /q "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Cache" 2>nul
 )
@@ -79,7 +79,7 @@ if exist "%LOCALAPPDATA%\Google\Chrome\User Data\Default\Code Cache" (
 )
 
 :: --- Microsoft Edge cache ---
-echo [7/14] Microsoft Edge cache...
+echo [7/13] Microsoft Edge cache...
 if exist "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Cache" (
     rd /s /q "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Cache" 2>nul
 )
@@ -88,28 +88,33 @@ if exist "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Code Cache" (
 )
 
 :: --- Firefox cache ---
-echo [8/14] Firefox cache...
+echo [8/13] Firefox cache...
 if exist "%LOCALAPPDATA%\Mozilla\Firefox\Profiles" (
     for /d %%P in ("%LOCALAPPDATA%\Mozilla\Firefox\Profiles\*") do (
         if exist "%%P\cache2" rd /s /q "%%P\cache2" 2>nul
     )
 )
 
-:: --- Microsoft Excel: recente bestanden lijst ---
-echo [9/14] Excel recente bestanden lijst wissen...
+:: --- Microsoft Excel: factory reset (account blijft bewaard) ---
+echo [9/13] Excel afsluiten...
+taskkill /f /im excel.exe 2>nul
+
+echo [10/13] Excel factory reset (inloggegevens blijven bewaard)...
+reg delete "HKCU\Software\Microsoft\Office\16.0\Excel" /f 2>nul
+reg delete "HKCU\Software\Microsoft\Office\15.0\Excel" /f 2>nul
+reg delete "HKCU\Software\Microsoft\Office\16.0\Common\Toolbars\Excel" /f 2>nul
+reg delete "HKCU\Software\Microsoft\Office\15.0\Common\Toolbars\Excel" /f 2>nul
+if exist "%APPDATA%\Microsoft\Excel\XLSTART" (
+    del /f /q "%APPDATA%\Microsoft\Excel\XLSTART\*.*" 2>nul
+)
 if exist "%APPDATA%\Microsoft\Excel" (
     del /f /q "%APPDATA%\Microsoft\Excel\*.xlb" 2>nul
-)
-
-:: --- Microsoft Excel: AutoRecover en temp bestanden ---
-echo [10/14] Excel AutoRecover bestanden wissen...
-if exist "%APPDATA%\Microsoft\Excel" (
     del /f /q "%APPDATA%\Microsoft\Excel\*.xar" 2>nul
     del /f /q "%APPDATA%\Microsoft\Excel\*.tmp" 2>nul
 )
 
 :: --- Office cache ---
-echo [11/14] Office cache (OfficeFileCache) wissen...
+echo [11/13] Office cache (OfficeFileCache) wissen...
 if exist "%LOCALAPPDATA%\Microsoft\Office\16.0\OfficeFileCache" (
     rd /s /q "%LOCALAPPDATA%\Microsoft\Office\16.0\OfficeFileCache" 2>nul
 )
@@ -122,18 +127,12 @@ if exist "%LOCALAPPDATA%\Microsoft\Office\UnsavedFiles" (
     del /f /q "%LOCALAPPDATA%\Microsoft\Office\UnsavedFiles\*.xlsm" 2>nul
 )
 
-:: --- Excel instellingen resetten (account/login blijft bewaard) ---
-echo [12/14] Excel instellingen resetten (account blijft bewaard)...
-taskkill /f /im excel.exe 2>nul
-reg delete "HKCU\Software\Microsoft\Office\16.0\Excel" /f 2>nul
-reg delete "HKCU\Software\Microsoft\Office\15.0\Excel" /f 2>nul
-
 :: --- Prullenbak leegmaken ---
-echo [13/14] Prullenbak leegmaken...
+echo [12/13] Prullenbak leegmaken...
 rd /s /q "C:\$Recycle.Bin" 2>nul
 
 :: --- Windows taal instellen op Engels (United States) ---
-echo [14/14] Windows taal instellen op Engels (United States)...
+echo [13/13] Windows taal instellen op Engels (United States)...
 powershell -NoProfile -Command "Set-WinUILanguageOverride -Language 'en-US'; Set-WinUserLanguageList 'en-US' -Force; Set-WinSystemLocale 'en-US'; Set-Culture 'en-US'" 2>nul
 
 echo.
